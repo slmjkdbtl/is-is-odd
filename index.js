e019e946b85601992ab3f4f85dab64b6f5119c89
tga <tga@enemyspy.xyz>
2019-01-07 20:42:06 -0500

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+// wengwengweng
+
+'use strict';
+
+const isOdd = require("is-odd");
+
+module.exports = function isIsOdd(func) {
+	return func == isOdd;
+}
+
---
+++ test.js
@@ -0,0 +1,27 @@
+// wengwengweng
+
+'use strict';
+
+require("mocha");
+const assert = require("assert");
+const isOdd = require("is-odd");
+const isIsOdd = require("./");
+
+describe("isOdd", function() {
+
+	it("should return true if a function is is-odd:", function() {
+		assert(isIsOdd(isOdd));
+	});
+
+	it("should return false if a function is not is-odd", function() {
+		assert(!isIsOdd(function() {}));
+	});
+
+	it("should return false even if a function is in disguise", function() {
+		assert(!isIsOdd(function() {
+			console.log("I'm is-odd");
+		}));
+	});
+
+});
+
